03611376a2b8e36bfa72935651c88b4155b64713
Codemaverick07 <hemantjatal59@gmail.com>
2024-07-08 14:45:52 +0530

    chore: Update Prisma client import in db/index.js
    The import statement for the PrismaClient in db/index.js was updated to use the named import syntax from the "@prisma/client" package. This change ensures that the correct import is used for the Prisma client.

--- db/index.js
+++ db/index.js
@@ -1,6 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const client_1 = require("@prisma/client");
-const client = new client_1.PrismaClient();
-exports.db = client;
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export const db = client;
