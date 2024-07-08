02be3c9bd3267d12cf39d1f0253c1f0a9e5a0fe2
Codemaverick07 <hemantjatal59@gmail.com>
2024-07-08 14:17:48 +0530

    chore: Update package.json and dockerfile

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const v1_1 = __importDefault(require("./router/v1"));
@@ -13,27 +15,31 @@ const express_session_1 = __importDefault(require("express-session"));
 const passport_2 = __importDefault(require("passport"));
 const app = (0, express_1.default)();
 dotenv_1.default.config();
-app.use((0, express_session_1.default)({
+app.use(
+  (0, express_session_1.default)({
     secret: process.env.COOKIE_SECRET || "keyboard cat",
     resave: false,
     saveUninitialized: false,
     cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
-}));
+  })
+);
 (0, passport_1.initPassport)();
 app.use(passport_2.default.initialize());
 app.use(passport_2.default.authenticate("session"));
 app.get("/", (req, res) => res.send("Welcome to chessAPI API"));
 const allowedHosts = process.env.ALLOWED_HOSTS
-    ? process.env.ALLOWED_HOSTS.split(",")
-    : [];
-app.use((0, cors_1.default)({
+  ? process.env.ALLOWED_HOSTS.split(",")
+  : [];
+app.use(
+  (0, cors_1.default)({
     origin: allowedHosts,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
-}));
+  })
+);
 app.use("/auth", auth_1.default);
 app.use("/v1", v1_1.default);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
